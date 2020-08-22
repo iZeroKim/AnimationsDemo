bd83c1bf75daebbb6df694076b4b6cca06daf9e1
iZeroKim <Kimgichuru6@gmail.com>
2020-08-22 13:53:55 +0300

    Feat: Added animation events listeners to all animations

--- app/src/main/java/com/sriyank/animationsdemo/MainActivity.kt
+++ app/src/main/java/com/sriyank/animationsdemo/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.sriyank.animationsdemo
 
+import android.animation.Animator
 import android.animation.AnimatorInflater
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Animator.AnimatorListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,8 +18,10 @@ class MainActivity : AppCompatActivity() {
         val rotateAnimator = AnimatorInflater.loadAnimator(this, R.animator.rotate)
         rotateAnimator?.apply {
             setTarget(targetImage)
+            addListener(this@MainActivity)
             start()
         }
+
     }
 
     fun scaleAnimation(view: View) {
@@ -25,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         val scaleAnimator = AnimatorInflater.loadAnimator(this, R.animator.scale)
         scaleAnimator?.apply {
             setTarget(targetImage)
+            addListener(this@MainActivity)
             start()
         }
     }
@@ -34,6 +39,7 @@ class MainActivity : AppCompatActivity() {
         val translateAnimator = AnimatorInflater.loadAnimator(this, R.animator.translate)
         translateAnimator.apply {
             setTarget(targetImage)
+            addListener(this@MainActivity)
             start()
         }
     }
@@ -43,7 +49,25 @@ class MainActivity : AppCompatActivity() {
         val fadeAnimator = AnimatorInflater.loadAnimator(this, R.animator.alpha)
         fadeAnimator.apply {
             setTarget(targetImage)
+            addListener(this@MainActivity)
             start()
         }
+    }
+
+    //Listening to Animation Events
+    override fun onAnimationStart(p0: Animator?) {
+        Toast.makeText(this, "Animation Started", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onAnimationEnd(p0: Animator?) {
+        Toast.makeText(this, "Animation Ended", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onAnimationCancel(p0: Animator?) {
+        Toast.makeText(this, "Animation Cancelled", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onAnimationRepeat(p0: Animator?) {
+        Toast.makeText(this, "Animation Repeated", Toast.LENGTH_SHORT).show()
     }
 }
